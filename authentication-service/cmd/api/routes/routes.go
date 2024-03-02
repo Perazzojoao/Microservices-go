4d88ac1295f9b2348b0bbdc8b86f0fb3cfa4410d
Perazzojoao <perazzojoao@gmail.com>
2024-03-02 17:13:09 -0300

    Adiciona arquivo de rotas para serviço de autenticação

---
+++ authentication-service/cmd/api/routes/routes.go
@@ -0,0 +1,37 @@
+package routes
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+
+	"authentication/data"
+
+)
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func (app *Config) Routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// Especificar permissões de CORS
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	// Routes
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	return mux
+}
