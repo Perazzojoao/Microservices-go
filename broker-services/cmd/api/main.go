0ebbe935b342b55fec32a303c97d8666235dc310
Perazzojoao <perazzojoao@gmail.com>
2024-03-01 22:46:29 -0300

    Adiciona configuração do servidor HTTP na porta 80

--- broker-services/cmd/api/main.go
+++ broker-services/cmd/api/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"broker/cmd/api/routes"
+
+)
+
+const webPort = "80"
+
+func main() {
+	app := routes.Config{}
+
+	log.Printf("Server is running on port:%s\n", webPort)
+
+	// Define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.Routes(),
+	}
+
+	// Start server
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
