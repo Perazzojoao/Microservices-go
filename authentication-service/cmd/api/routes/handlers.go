e1b06108803e35a63f571e0fd02b60a48b1e8a38
Perazzojoao <perazzojoao@gmail.com>
2024-03-04 00:54:07 -0300

    Atualizações no manipulador de autenticação

--- authentication-service/cmd/api/routes/handlers.go
+++ authentication-service/cmd/api/routes/handlers.go
@@ -1,15 +1,41 @@
 package routes
 
-import "net/http"
+import (
+	"errors"
+	"fmt"
+	"net/http"
 
-func (app *Config) Authentication(w http.ResponseWriter, r *http.Request) {
-	payload := jsonResponse{
-		Error:   false,
-		Message: "Hello, World!",
+)
+
+func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
+	var requestPayload struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
 	}
 
-	err := app.writeJSON(w, http.StatusOK, payload)
+	err := app.readJson(w, r, &requestPayload)
 	if err != nil {
 		app.errorJSON(w, err)
+		return
 	}
+
+	user, err := app.Models.User.GetByEmail(requestPayload.Email)
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid email or password"))
+		return
+	}
+
+	valid, err := user.PasswordMatches(requestPayload.Password)
+	if err != nil || !valid {
+		app.errorJSON(w, errors.New("invalid email or password"))
+		return
+	}
+
+	payload := jsonResponse{
+		Error:  false,
+		Message: fmt.Sprintf("Logged in as %s", user.Email),
+		Data: 	user,
+	}
+
+	app.writeJSON(w, http.StatusOK, payload)
 }
