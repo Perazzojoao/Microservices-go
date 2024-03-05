6157fe62f327dccfbe9941e523a35c0379f6b565
Perazzojoao <perazzojoao@gmail.com>
2024-03-04 23:59:12 -0300

    Adiciona rota para gravar logs

---
+++ logger-service/cmd/api/routes/handlers.go
@@ -0,0 +1,37 @@
+package routes
+
+import (
+	"net/http"
+
+	"log-service/data"
+)
+
+type JSONPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (app *Config) WriteLog(w http.ResponseWriter, r *http.Request) {
+	// Ler json do corpo da requisição
+	var requestPayload JSONPayload
+	_ = app.readJson(w, r, &requestPayload)
+
+	// Inserindo dados
+	event := data.LogEntry{
+		Name: requestPayload.Name,
+		Data: requestPayload.Data,
+	}
+
+	err := app.Models.LogEntry.Insert(event)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	resp := jsonResponse{
+		Error:   false,
+		Message: "logged",
+	}
+
+	app.writeJSON(w, http.StatusAccepted, resp)
+}
