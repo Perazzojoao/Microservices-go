a1fd009066a32bef8949017c5ecda81a6c6604fd
Perazzojoao <perazzojoao@gmail.com>
2024-03-12 00:29:05 -0300

    Adiciona funcionalidade de log

--- broker-services/cmd/api/routes/handlers.go
+++ broker-services/cmd/api/routes/handlers.go
@@ -5,16 +5,23 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
+
 )
 
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -41,8 +48,51 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch RequestPayload.Action {
 	case "auth":
 		app.authenticate(w, RequestPayload.Auth)
+	case "log":
+		app.logItem(w, RequestPayload.Log)
 	default:
 		app.errorJSON(w, errors.New("invalid action"))
+	}
+}
+
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	// Criar um JSON para enviar ao serviço de log
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	loggerServiceURL := "http://logger-service/log"
+
+	// Enviar o JSON para o serviço de log
+	request, err := http.NewRequest("POST", loggerServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	// Receber a resposta do serviço de log com o statuscode correto
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error calling log service"), http.StatusInternalServerError)
+		return
+	}
+
+	// Criar uma vaeiável para armazenar a resposta do serviço de logger
+	var payload = jsonResponse{
+		Error:   false,
+		Message: "Logged",
+	}
+
+	// Escrever a resposta da requisição no formato JSON e definir o status da resposta
+	err = app.writeJSON(w, http.StatusOK, payload)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusInternalServerError)
 	}
 }
 
