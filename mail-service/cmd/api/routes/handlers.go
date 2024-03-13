33d9e2623ec5b47a91e8556b03039139fdcb42e2
Perazzojoao <perazzojoao@gmail.com>
2024-03-13 15:06:19 -0300

    Adiciona manipulador para envio de e-mails

---
+++ mail-service/cmd/api/routes/handlers.go
@@ -0,0 +1,48 @@
+package routes
+
+import (
+	"net/http"
+
+	"mailer-service/cmd/api/mailer"
+
+)
+
+func (app *Config) SendMail(w http.ResponseWriter, r *http.Request) {
+	type mailMessage struct {
+		From    string `json:"from"`
+		To      string `json:"to"`
+		Subject string `json:"subject"`
+		Message string `json:"message"`
+	}
+
+	// Lendo e armazenando os dados da requisição em requestPayload
+	var requestPayload mailMessage
+	err := app.readJson(w, r, &requestPayload)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	// Criando mensagem
+	msg := mailer.Message{
+		From:    requestPayload.From,
+		To:      requestPayload.To,
+		Subject: requestPayload.Subject,
+		Data:    requestPayload.Message,
+	}
+
+	// Enviando email para destinatário
+	err = app.Mailer.SendSMTPMessage(msg)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	// Retornando resposta da requisição
+	payload := jsonResponse {
+		Error: false,
+		Message: "sent to " + requestPayload.To,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
