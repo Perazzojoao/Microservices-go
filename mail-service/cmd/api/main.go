86316f26247531f3cca3e26579a9e1944dd123d9
Perazzojoao <perazzojoao@gmail.com>
2024-03-13 15:06:31 -0300

    Adiciona configuração do serviço de envio de e-mails

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"mailer-service/cmd/api/mailer"
 	"mailer-service/cmd/api/routes"
+
 )
 
 func main() {
-	app := routes.Config{}
+	app := routes.Config{
+		Mailer: mailer.CreateMail(),
+	}
 
 	app.Serve()
 }
