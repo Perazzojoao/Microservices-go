41015d54a290f82edcdc51da75c70b6046b14406
Perazzojoao <perazzojoao@gmail.com>
2024-03-13 15:05:52 -0300

    Adiciona arquivo createMailer.go para criar objeto Mailer

---
+++ mail-service/cmd/api/mailer/createMailer.go
@@ -0,0 +1,22 @@
+package mailer
+
+import (
+	"os"
+	"strconv"
+)
+
+func CreateMail() *Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Passwd:      os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("FROM_NAME"),
+		FromAddress: os.Getenv("FROM_ADDRESS"),
+	}
+
+	return &m
+}
