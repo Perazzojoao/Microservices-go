7f0c712fefc632cfb02df363634b08f036f08acf
Perazzojoao <perazzojoao@gmail.com>
2024-03-18 12:54:15 -0300

    Adiciona conexão com RabbitMQ e esqueleto para ouvir fila de mensagens e enviar para servidor HTTP

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"listener/database"
+)
+
+func main() {
+	// Conectar com rabbitmq
+	rabbitConn, err := database.Connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+	log.Println("Connected to RabbitMQ")
+
+	//Começar a ouvir a fila de mensagens
+
+	// Criar um servidor http
+
+	// Observar fila de mensagens e enviar para o servidor http
 }
